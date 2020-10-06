997b14fe4d07da25499b016a3e0a6c8d1320a682
TylerTam <46612488+TylerTam@users.noreply.github.com>
2020-10-06 16:47:31 -0400

    Added player toggle input
    Script that contains all player inputs to toggle on/off

---
+++ Assets/_GreenLeaves/Scripts/Player/PlayerInputToggle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputToggle : MonoBehaviour
+{
+    public static PlayerInputToggle Instance;
+    public Cinemachine.CinemachineFreeLook m_cameraRotation;
+    public PlayerInput m_playerInput;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void ToggleInput(bool p_newState)
+    {
+        m_playerInput.enabled = p_newState;
+        m_cameraRotation.enabled = p_newState;
+    }
+}
