f95688a346f47543f76b2b5cd4a8607d4ee46735
Sebastian_Swarney <sebastian@builtbysnowman.com>
2020-11-09 12:32:05 -0500

    First pass on POI system

---
+++ Assets/_GreenLeaves/Scripts/Collectables/POIController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class POIController : MonoBehaviour
+{
+	public LayerMask m_playerMask;
+
+    public GameObject m_poiFoundCam;
+
+    private bool m_discovered;
+
+    private void OnObjectEnter(GameObject p_object)
+	{
+        if (CheckCollisionLayer(m_playerMask, p_object))
+        {
+            if (!m_discovered)
+            {
+                OnPlayerDiscorvered();
+            }
+        }
+	}
+
+    private void OnObjectLeave(GameObject p_object)
+	{
+        if (CheckCollisionLayer(m_playerMask, p_object))
+        {
+            if (m_discovered)
+            {
+                m_poiFoundCam.SetActive(false);
+            }
+        }
+    }
+
+    private void OnPlayerDiscorvered()
+	{
+        m_discovered = true;
+        Debug.Log("Player found point");
+        m_poiFoundCam.SetActive(true);
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+        OnObjectEnter(other.gameObject);
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+        OnObjectLeave(other.gameObject);
+    }
+
+	public bool CheckCollisionLayer(LayerMask p_layerMask, GameObject p_object)
+    {
+        if (p_layerMask == (p_layerMask | (1 << p_object.layer)))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
