a92104f542689d1bbbeb6dd8cd3e005fa80be027
TylerTam <46612488+TylerTam@users.noreply.github.com>
2020-11-23 14:30:15 -0500

    Added FMOD
    Added Previous Sounds

---
+++ Assets/_GreenLeaves/Scripts/Sound/SoundEmitter_FootSteps.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEmitter_FootSteps : MonoBehaviour
+{
+
+    [FMODUnity.EventRef]
+    public string[] m_soundEffect;
+
+    public FMODUnity.StudioEventEmitter m_eventEmitter;
+
+    public bool m_debugSound;
+    public int m_currentSound;
+    private int m_currentSoundTrack;
+
+    private void OnValidate()
+    {
+        if (m_debugSound)
+        {
+            m_debugSound = false;
+            PlaySound(m_currentSound);
+        }
+    }
+    public void PlaySound(int p_soundType)
+    {
+        //Debug.Log()
+        m_eventEmitter.Event = m_soundEffect[p_soundType];
+        if(m_currentSoundTrack != p_soundType)
+        {
+            m_eventEmitter.Lookup();
+        }
+        m_currentSoundTrack = p_soundType;
+        m_eventEmitter.Play();
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlaySound(m_currentSound);
+    }
+}
