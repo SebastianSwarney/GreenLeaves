a1a77f1a041c61ec9acc99f1b2c1aedcbe10ffb5
TylerTam <46612488+TylerTam@users.noreply.github.com>
2020-11-30 14:56:47 -0500

    Added sound prefab for tutorial level
    Created footstep sound changer

---
+++ Assets/_GreenLeaves/Scripts/Sound/SoundChanger_FootSteps.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundChanger_FootSteps : MonoBehaviour
+{
+    public int m_soundTypeIndex;
+    public int m_specialSoundIndex;
+    public float m_specialSoundChance;
+
+    private int m_soundLayer;
+    
+
+    private void Start()
+    {
+        m_soundLayer = LayerMask.NameToLayer("Sounds");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == m_soundLayer)
+        {
+            if(other.gameObject.GetComponent<SoundEmitter_FootSteps>() != null)
+            {
+                SoundEmitter_FootSteps newStep = other.gameObject.GetComponent<SoundEmitter_FootSteps>();
+                newStep.m_currentSound = m_soundTypeIndex;
+                newStep.SwapTwigEffect(m_specialSoundIndex, m_specialSoundChance);
+            }
+        }
+    }
+}
