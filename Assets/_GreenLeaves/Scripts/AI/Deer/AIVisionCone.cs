4a0c0e4644eed9081ece398d09966b7c8dbdcf68
TylerTam <46612488+TylerTam@users.noreply.github.com>
2021-02-10 17:45:17 -0500

    Initial Deer AI

---
+++ Assets/_GreenLeaves/Scripts/AI/Deer/AIVisionCone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class VisionEvent : UnityEngine.Events.UnityEvent { }
+public class AIVisionCone : MonoBehaviour
+{
+    public VisionEvent m_playerEnteredCone;
+
+    public string m_playerTag;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == m_playerTag)
+        {
+            m_playerEnteredCone.Invoke();
+        }
+    }
+}
