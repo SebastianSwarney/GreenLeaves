72cbe49f62f74a05c73ef5e3091ee4837ae033b2
TylerTam <46612488+TylerTam@users.noreply.github.com>
2021-02-17 11:46:50 -0500

    Update Crafting_ToolComponents.cs

--- Assets/_GreenLeaves/Scripts/Resource Systems/Crafting/Crafting_ToolComponents.cs
+++ Assets/_GreenLeaves/Scripts/Resource Systems/Crafting/Crafting_ToolComponents.cs
@@ -3,7 +3,6 @@
 public class Crafting_ToolComponents : MonoBehaviour
 {
     public Inventory_Icon_ToolResource m_axeResource, m_torchResource, m_canteenResource, m_bowResource, m_climbingAxeResource;
-    public Vector3 m_axePos, m_torchPos, m_canteenPos, m_bowPos, m_climbingAxePos;
 
     private void Start()
     {
@@ -50,27 +49,25 @@ public class Crafting_ToolComponents : MonoBehaviour
         switch (p_toolType)
         {
             case ResourceContainer_Equip.ToolType.Axe:
-                m_axeResource.transform.localPosition = m_axePos;
+
                 m_axeResource.gameObject.SetActive(true);
                 break;
 
             case ResourceContainer_Equip.ToolType.Torch:
-                m_torchResource.transform.localPosition = m_torchPos;
+
                 m_torchResource.gameObject.SetActive(true);
                 break;
 
             case ResourceContainer_Equip.ToolType.Canteen:
-                m_canteenResource.transform.localPosition = m_canteenPos;
+
                 m_canteenResource.gameObject.SetActive(true);
                 break;
 
             case ResourceContainer_Equip.ToolType.Bow:
-                m_bowResource.transform.localPosition = m_bowPos;
                 m_bowResource.gameObject.SetActive(true);
                 break;
 
             case ResourceContainer_Equip.ToolType.ClimbingAxe:
-                m_climbingAxeResource.transform.localPosition = m_climbingAxePos;
                 m_climbingAxeResource.gameObject.SetActive(true);
                 break;
         }
