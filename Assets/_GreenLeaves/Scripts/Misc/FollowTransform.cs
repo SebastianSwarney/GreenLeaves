d89c49472b912087728289b1e1b276958552eaf2
TylerTam <46612488+TylerTam@users.noreply.github.com>
2021-03-11 16:59:34 -0500

    Heres the physics object on the bag, but it doesnt work ¯\_(ツ)_/¯

---
+++ Assets/_GreenLeaves/Scripts/Misc/FollowTransform.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTransform : MonoBehaviour
+{
+    public Transform m_followTransform;
+    public Vector3 m_offset;
+
+    public bool m_debug;
+    public Color m_debugColor;
+
+    private void Awake()
+    {
+        if (m_followTransform == null) gameObject.SetActive(false);
+    }
+    private void Update()
+    {
+        transform.position = m_followTransform.position + m_offset;
+    }
+    private void OnDrawGizmos()
+    {
+        if (m_followTransform == null) return;
+        if (!m_debug) return;
+        Gizmos.color = m_debugColor;
+        Gizmos.DrawWireSphere(m_followTransform.position + m_offset, 0.025f);
+    }
+}
