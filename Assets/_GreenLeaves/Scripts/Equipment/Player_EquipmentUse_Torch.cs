2978a5abe90c47c840c591fcc7a690e4d6731866
TylerTam <46612488+TylerTam@users.noreply.github.com>
2021-03-22 13:01:52 -0400

    Push torch singleton

---
+++ Assets/_GreenLeaves/Scripts/Equipment/Player_EquipmentUse_Torch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_EquipmentUse_Torch : Player_EquipmentUse
+{
+    public static Player_EquipmentUse_Torch Instance;
+
+    public bool m_torchEquipped;
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public override void ObjectBroke()
+    {
+        base.ObjectBroke();
+        m_torchEquipped = false;
+    }
+
+
+    public override void EquipObject(Inventory_Icon_Durability p_linkedIcon)
+    {
+        base.EquipObject(p_linkedIcon);
+        m_torchEquipped = true;
+    }
+    public override void UnEquipObject()
+    {
+        base.UnEquipObject();
+        m_torchEquipped = false;
+    }
+
+}
