110298aa32df9a52de68309fc2cc5d5da40ba79d
TylerTam <46612488+TylerTam@users.noreply.github.com>
2021-03-22 13:16:20 -0400

    Update torch to have reducing durability

--- Assets/_GreenLeaves/Scripts/Equipment/Player_EquipmentUse_Torch.cs
+++ Assets/_GreenLeaves/Scripts/Equipment/Player_EquipmentUse_Torch.cs
@@ -7,6 +7,10 @@ public class Player_EquipmentUse_Torch : Player_EquipmentUse
     public static Player_EquipmentUse_Torch Instance;
 
     public bool m_torchEquipped;
+
+    public int m_timeToLoseOneDurability = 1;
+
+    private float m_timer = 0;
     private void Awake()
     {
         Instance = this;
@@ -18,16 +22,29 @@ public class Player_EquipmentUse_Torch : Player_EquipmentUse
         m_torchEquipped = false;
     }
 
-
+    private void Update()
+    {
+        if (m_torchEquipped && !Inventory_2DMenu.Instance.m_isOpen && !PlayerUIManager.Instance.m_isPaused)
+        {
+            m_timer += Time.deltaTime;
+            if(m_timer > m_timeToLoseOneDurability)
+            {
+                ReduceDurability();
+                m_timer = 0;
+            }
+        }
+    }
     public override void EquipObject(Inventory_Icon_Durability p_linkedIcon)
     {
         base.EquipObject(p_linkedIcon);
         m_torchEquipped = true;
+        m_timer = 0;
     }
     public override void UnEquipObject()
     {
         base.UnEquipObject();
         m_torchEquipped = false;
     }
+
 
 }
