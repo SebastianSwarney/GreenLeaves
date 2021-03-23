7b3e90276e6957399f882a014be5c15d4ed78e6e
TylerTam <46612488+TylerTam@users.noreply.github.com>
2021-03-23 12:33:41 -0400

    Adjusted music to fade between different tracks now
    Implemented current tracks

--- Assets/_GreenLeaves/Scripts/Sound/MusicPlayer.cs
+++ Assets/_GreenLeaves/Scripts/Sound/MusicPlayer.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
 {
+
+    public static MusicPlayer Instance;
     public float m_minTimeBetweenTracks, m_maxTimeBetweenTracks;
 
     private float m_currentTimer, m_currentRandomTime;
@@ -24,6 +26,25 @@ public class MusicPlayer : MonoBehaviour
         [FMODUnity.EventRef]
         public string m_daytimeBasedSongEvent;
         public float m_minDaytime, m_maxDaytime;
+    }
+
+
+    [Header("Runtime Song States")]
+    public MusicPlayer_Trigger.MusicTriggerType m_currentMusicType;
+    [Header("Summit Song")]
+    [FMODUnity.EventRef]
+    public string m_summitSongEvent;
+    public bool m_summitSongPlaying;
+
+    [Header("Climbing Song")]
+    [FMODUnity.EventRef]
+    public string m_climbingSongEvent;
+    public bool m_climbingSongPlaying;
+
+
+    private void Awake()
+    {
+        Instance = this;
     }
     private void Start()
     {
@@ -61,13 +82,17 @@ public class MusicPlayer : MonoBehaviour
     }
     private void Update()
     {
+
+
         if (m_delayingInitialTime) return;
-        if (!m_emitter.IsPlaying())
+        if (!m_emitter.IsPlaying() && !m_summitSongPlaying)
         {
             m_currentTimer += Time.deltaTime;
             if (m_currentTimer > m_currentRandomTime)
             {
                 string current = GetCurrentSongType();
+
+
                 if (current != "oof")
                 {
                     if (m_emitter.Event != current)
@@ -76,6 +101,7 @@ public class MusicPlayer : MonoBehaviour
                         m_emitter.Lookup();
                     }
                     m_emitter.Play();
+                    m_emitter.AllowFadeout = false;
                 }
                 else
                 {
@@ -85,6 +111,7 @@ public class MusicPlayer : MonoBehaviour
             }
         }
     }
+
 
     public string GetCurrentSongType()
     {
@@ -109,5 +136,85 @@ public class MusicPlayer : MonoBehaviour
         }
 
         return "oof";
+    }
+
+    public void ChangeSong(MusicPlayer_Trigger.MusicTriggerType p_newSongType)
+    {
+
+        switch (p_newSongType)
+        {
+            case MusicPlayer_Trigger.MusicTriggerType.Exploration:
+                StartCoroutine(FadeOutCurrentMusic(GetCurrentSongType(), p_newSongType));
+                break;
+            case MusicPlayer_Trigger.MusicTriggerType.Climbing:
+                StartCoroutine(FadeOutCurrentMusic(m_climbingSongEvent, p_newSongType));
+                break;
+            case MusicPlayer_Trigger.MusicTriggerType.Summit:
+                StartCoroutine(FadeOutCurrentMusic(m_summitSongEvent, p_newSongType));
+                break;
+        }
+
+    }
+
+
+    private IEnumerator FadeOutCurrentMusic(string p_currentEvent, MusicPlayer_Trigger.MusicTriggerType p_newSongType)
+    {
+        bool playing = false;
+
+        switch (p_newSongType)
+        {
+            case MusicPlayer_Trigger.MusicTriggerType.Exploration:
+                break;
+            case MusicPlayer_Trigger.MusicTriggerType.Climbing:
+                m_climbingSongPlaying = true;
+                break;
+            case MusicPlayer_Trigger.MusicTriggerType.Summit:
+                m_summitSongPlaying = true;
+                break;
+        }
+
+        m_currentMusicType = p_newSongType;
+
+        m_summitSongPlaying = true;
+
+
+        if (m_emitter.IsPlaying())
+        {
+            playing = true;
+            m_emitter.AllowFadeout = true;
+            m_emitter.Stop();
+        }
+
+
+        while (playing)
+        {
+            yield return null;
+            if (!m_emitter.IsPlaying())
+            {
+                playing = false;
+            }
+        }
+        m_emitter.Event = p_currentEvent;
+        m_emitter.Lookup();
+        m_emitter.Play();
+
+
+
+        switch (p_newSongType)
+        {
+            case MusicPlayer_Trigger.MusicTriggerType.Exploration:
+                m_climbingSongPlaying = false;
+                m_summitSongPlaying = false;
+                m_currentTimer = 0;
+                break;
+            case MusicPlayer_Trigger.MusicTriggerType.Climbing:
+                m_climbingSongPlaying = true;
+                m_summitSongPlaying = false;
+                break;
+            case MusicPlayer_Trigger.MusicTriggerType.Summit:
+                m_summitSongPlaying = true;
+                m_climbingSongPlaying = false;
+                break;
+        }
     }
 }
---
+++ Assets/_GreenLeaves/Scripts/Sound/MusicPlayer_Trigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer_Trigger : MonoBehaviour
+{
+    public enum MusicTriggerType { Summit, Climbing, Exploration }
+    public MusicTriggerType m_triggerType;
+
+    public string m_playerTag;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == m_playerTag)
+        {
+            MusicPlayer.Instance.ChangeSong(m_triggerType);
+        }
+    }
+}
