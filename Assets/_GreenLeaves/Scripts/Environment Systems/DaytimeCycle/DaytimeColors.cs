33d57e197a7b36960afa39fdb74e5e147cefd356
TylerTam <46612488+TylerTam@users.noreply.github.com>
2021-03-25 12:30:19 -0400

    Fixed issue with daytime changing while in cave

--- Assets/_GreenLeaves/Scripts/Environment Systems/DaytimeCycle/DaytimeColors.cs
+++ Assets/_GreenLeaves/Scripts/Environment Systems/DaytimeCycle/DaytimeColors.cs
@@ -6,7 +6,7 @@ public class DaytimeColors : ScriptableObject
 {
     public List<DayColors> m_dayColors;
 
-    
+
     public DayColors m_caveColor;
 
     [System.Serializable]
@@ -25,15 +25,15 @@ public class DaytimeColors : ScriptableObject
         float percent = 0;
         if (p_currentTime < m_dayColors[0].m_timeOfDay)
         {
-            pastColor = m_dayColors[m_dayColors.Count-1];
+            pastColor = m_dayColors[m_dayColors.Count - 1];
             currentColor = m_dayColors[0];
 
             percent = (pastColor.m_timeOfDay + p_currentTime) / (pastColor.m_timeOfDay + currentColor.m_timeOfDay);
         }
-        else if (p_currentTime > m_dayColors[m_dayColors.Count-1].m_timeOfDay)
+        else if (p_currentTime > m_dayColors[m_dayColors.Count - 1].m_timeOfDay)
         {
             pastColor = m_dayColors[0];
-            currentColor = m_dayColors[m_dayColors.Count-1];
+            currentColor = m_dayColors[m_dayColors.Count - 1];
 
             percent = (pastColor.m_timeOfDay + p_currentTime) / (pastColor.m_timeOfDay + currentColor.m_timeOfDay);
         }
@@ -58,10 +58,9 @@ public class DaytimeColors : ScriptableObject
         RenderSettings.ambientEquatorColor = Color.Lerp(Color.Lerp(pastColor.m_equatorColor, currentColor.m_equatorColor, percent), m_caveColor.m_equatorColor, p_cavePercent);
         RenderSettings.ambientGroundColor = Color.Lerp(Color.Lerp(pastColor.m_groundColor, currentColor.m_groundColor, percent), m_caveColor.m_groundColor, p_cavePercent);
         RenderSettings.ambientSkyColor = Color.Lerp(Color.Lerp(pastColor.m_skyColor, currentColor.m_skyColor, percent), m_caveColor.m_skyColor, p_cavePercent);
-        if (!p_inCave)
-        {
-            p_morningLight.intensity = Mathf.Lerp(pastColor.m_morningLightIntensity, currentColor.m_morningLightIntensity, percent);
-            p_nightLight.intensity = Mathf.Lerp(pastColor.m_nightLightIntensity, currentColor.m_nightLightIntensity, percent);
-        }
+
+        p_morningLight.intensity = Mathf.Lerp(Mathf.Lerp(pastColor.m_morningLightIntensity, currentColor.m_morningLightIntensity, percent), m_caveColor.m_morningLightIntensity, p_cavePercent);
+        p_nightLight.intensity = Mathf.Lerp(Mathf.Lerp(pastColor.m_nightLightIntensity, currentColor.m_nightLightIntensity, percent), m_caveColor.m_nightLightIntensity, p_cavePercent);
+
     }
 }
