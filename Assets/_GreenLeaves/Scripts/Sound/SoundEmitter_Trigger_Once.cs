c9b063113c641de403e9a252cf261e2aa78a62a0
TylerTam <46612488+TylerTam@users.noreply.github.com>
2021-04-06 11:13:36 -0400

    Added in new music for climb and skeleton

---
+++ Assets/_GreenLeaves/Scripts/Sound/SoundEmitter_Trigger_Once.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEmitter_Trigger_Once : MonoBehaviour
+{
+    public bool m_played = false;
+    public FMODUnity.StudioEventEmitter m_emitter;
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_played)
+        {
+            if (!m_emitter.IsPlaying())
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            m_emitter.Play();
+            m_played = true;
+        }
+    }
+}
+
